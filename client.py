76e3c8c88c1127327378978432b3c2fff0b4ead8
markeganfuller <mark.egan-fuller@mvfglobal.com>
2013-01-07 21:01:24 +0000

    Add message printing and peer removal

--- client.py
+++ client.py
@@ -9,8 +9,13 @@ def relay(queue, peers):
 		except Queue.Empty:
 			pass
 		else:
+			print message
+
 			for i in peers:
 				if i != message['last_node']:
 					url = "http://" + i + ":8000"
 					data = message['msg']
-					print urllib2.urlopen(url, data).read()
+					try:
+						print urllib2.urlopen(url, data).read()
+					except:
+						set.remove(i)
