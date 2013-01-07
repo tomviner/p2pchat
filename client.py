1bc1291c790b823b8bee35da3fbbcffb0ebb0fa7
markeganfuller <mark.egan-fuller@mvfglobal.com>
2013-01-07 20:48:30 +0000

    Added queue empty checking and loop.

--- client.py
+++ client.py
@@ -1,10 +1,16 @@
 import urllib2
+import Queue
 
 
 def relay(queue, peers):
-	message = queue.get(True, 2)  # Block for 2 seconds
-	for i in peers:
-		if i != message['last_node']:
-			url = "http://" + i + ":8000"
-			data = message['msg']
-			print urllib2.urlopen(url, data).read()
+	while True:
+		try:
+			message = queue.get(True, 2)  # Block for 2 seconds
+		except Queue.Empty:
+			pass
+		else:
+			for i in peers:
+				if i != message['last_node']:
+					url = "http://" + i + ":8000"
+					data = message['msg']
+					print urllib2.urlopen(url, data).read()
