aed73cefff31e6060e52b972f6f2f66ea69602dc
TomV <tom@viner.tv>
2013-01-07 21:02:42 +0000

    more chatage

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import threading
 import Queue
 
-from server import serve
 from client import relay
 from ui import chat
 
-
+def _serve(*a, **kw):
+    from server import serve
 
 def main():
     q = Queue.Queue()
     peers = set()
-    s = threading.Thread(target=serve, args=(q, peers)).start()
-    r = threading.Thread(target=requester, args=(q, peers)).start()
-    threading.Thread(target=chat, args=(q, peers), kwargs={threads=(s, r)}.start()
+    print 1
+    s = threading.Thread(target=_serve, args=(q, peers)).start()
+    print 2
+    r = threading.Thread(target=relay, args=(q, peers)).start()
+    print 3
+    threading.Thread(target=chat, args=(q, peers), kwargs={'threads':(s, r)}).start()
+    print 4
 
+if __name__ == '__main__':
+    main()
--- ui.py
+++ ui.py
@@ -1,13 +1,22 @@
 
 
-def chat(q, peers, threads):
+def chat(queue, peers, threads):
+    print 'starting chat ui'
     while True:
-        resp = ask("what is your message?")
         try:
-            return raw_input(prompt)
+            print 'you are connected to', peers
+            resp = raw_input("what is your message? ")
         except KeyboardInterrupt:
+            print 'QUIT'
             for thread in threads:
+                print 'attempt to kill', thread
                 thread.join()
             break
+        message = {
+            'msg': resp,
+            'last_node': None,
+        }
+        queue.put(message)
+
 
 
